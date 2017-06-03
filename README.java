ca8e320bd59a33d4cc469a874f4e711037366863
Ram734 <prasannaking48@gmail.com>
2017-06-03 07:24:04 +0530

    Create README.java

---
+++ README.java
@@ -0,0 +1,10 @@
+class reverse
+{
+public static void main(String args[])
+{
+String s=args[0];
+StringBuffer s1=new StringBuffer(s);
+String s2=new string(s1.reverse());
+System.out.print(s2);
+}
+}
